278013797b6658445a0c003cfd58ce303e3d8551
mohyhy <mtsmy31@gmail.com>
2024-12-10 00:00:02 +0000

    Automated commit

--- Aldemapp/urls.py
+++ Aldemapp/urls.py
@@ -4,6 +4,7 @@ from django.views.static import serve
 from django.conf import settings
 from django.contrib.sitemaps.views import sitemap
 from .sitemap import YourModelSitemap
+from django.contrib.auth import views as auth_views
 
 
 sitemaps = {
@@ -28,8 +29,10 @@ urlpatterns = [
     path('paid', views.paid ,name='paid'),
     path('pakages', views.pakages ,name='pakages'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-
-
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 
