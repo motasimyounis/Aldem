e42822633aea4d41563dd1b91327cc4e921f8d13
Motasim T Younis <106841046+motasimyounis@users.noreply.github.com>
2024-12-17 21:44:36 +0200

    Update admin.py

--- Aldemapp/admin.py
+++ Aldemapp/admin.py
@@ -6,7 +6,11 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from markdownx.widgets import MarkdownxWidget
-
+from django.core.mail import send_mail, send_mass_mail
+from django.core.exceptions import ValidationError
+from django.utils.text import Truncator
+from django.core.mail import send_mass_mail
+from django.conf import settings
 
 User = get_user_model()
 
@@ -119,5 +123,68 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Profile,ProfileAdmin)
+
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('short_message', 'user', 'is_for_all', 'is_read', 'created_at')
+    list_filter = ('is_for_all', 'is_read', 'created_at')
+    search_fields = ('message', 'user__email')
+    readonly_fields = ('is_read',)  # إضافة is_read للحقل القابل للقراءة فقط
+
+
+    # طريقة لعرض أول 20 كلمة من الرسالة باستخدام Truncator
+    def short_message(self, obj):
+        truncator = Truncator(obj.message)
+        return truncator.chars(50, truncate='...')  # تقطيع النص إلى 100 حرف فقط مع إضافة "..."
+
+    short_message.admin_order_field = 'message'  # لجعل الترتيب بناءً على الرسالة الأصلية
+    short_message.short_description = 'Message'  # تغيير عنوان العمود في الجدولMessage'  # تغيير عنوان العمود في الجدول
+
+
+    def save_model(self, request, obj, form, change):
+        # تحقق من أن أحد الخيارين تم تحديده
+        if not obj.user and not obj.is_for_all:
+            raise ValidationError("يجب تحديد إما طالب أو تحديد إشعار لجميع الطلاب.")
+        
+        if obj.is_for_all:  # إذا كان الإشعار لجميع المستخدمين
+            self.notify_all_users_with_email(obj.message)
+        else:  # إشعار فردي
+            super().save_model(request, obj, form, change)
+            if obj.user:  # إذا كان هناك مستخدم محدد
+                self.send_email_to_user(obj.user, obj.message)  # إرسال الإشعار بالبريد الإلكتروني
+
+
+    def notify_all_users(self, message):
+        users = User.objects.all()
+        notifications = [
+            Notification(user=user, message=message) for user in users
+        ]
+        Notification.objects.bulk_create(notifications)
+
+    def notify_all_users_with_email(self, message):
+        users = User.objects.exclude(email__isnull=True).exclude(email__exact='')  # فقط المستخدمين الذين لديهم بريد إلكتروني
+        # إنشاء الإشعارات
+        notifications = [
+            Notification(user=user, message=message) for user in users
+        ]
+        Notification.objects.bulk_create(notifications)
+
+        # إرسال البريد الإلكتروني
+        email_messages = [
+            ('إشعار جديد من منصة الأستاذة الدمرداش', message, settings.DEFAULT_FROM_EMAIL, [user.email]) for user in users
+        ]
+        send_mass_mail(email_messages, fail_silently=False)
+
+    def send_email_to_user(self, user, message):
+        if user.email:  # تأكد أن المستخدم لديه بريد إلكتروني
+            send_mail(
+                subject='إشعار جديد  من منصة الأستاذة الدمرداش',
+                message=message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[user.email],
+                fail_silently=False,  # اجعلها True لتجنب كسر العملية عند حدوث خطأ
+            )
 
 
